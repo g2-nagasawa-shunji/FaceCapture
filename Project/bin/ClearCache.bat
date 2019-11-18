c659536c271f78b16b28f04092588fc474bb3b1d
S_Nagasawa <nagasawa.shunji@geechs.com>
2019-11-18 16:43:21 +0900

    create bin

---
+++ Project/bin/ClearCache.bat
@@ -0,0 +1,19 @@
+rmdir /q /s ..\\Intermediate
+rmdir /q /s ..\\Saved\\Autosaves
+rmdir /q /s ..\\Saved\\Backup
+rmdir /q /s ..\\Saved\\Collections
+rmdir /q /s ..\\Saved\\Config
+rmdir /q /s ..\\Saved\\Crashes
+rmdir /q /s ..\\Saved\\Logs
+
+rmdir /q /s ..\\.vs
+rmdir /q /s ..\\Binaries
+
+rmdir /q /s ..\\Plugins\\KawaiiPhysics\\Binaries
+rmdir /q /s ..\\Plugins\\KawaiiPhysics\\Intermediate
+rmdir /q /s ..\\Plugins\\Swipe\\Binaries
+rmdir /q /s ..\\Plugins\\Swipe\\Intermediate
+del ..\\Flare.sln
+
+pause
+exit 0
---
+++ Project/bin/ClearCache.command
@@ -0,0 +1,18 @@
+rm -rf ../Intermediate
+rm -rf ../Saved/Autosaves
+rm -rf ../Saved/Backup
+rm -rf ../Saved/Collections
+rm -rf ../Saved/Config
+rm -rf ../Saved/Crashes
+rm -rf ../Saved/Logs
+rm -rf ../Saved/BinaryPSOs
+
+rm -rf ../Flare.xcworkspace
+rm -rf ../Binaries
+
+rm -rf ../Plugins/KawaiiPhysics/Binaries
+rm -rf ../Plugins/KawaiiPhysics/Intermediate
+rm -rf ../Plugins/Swipe/Binaries
+rm -rf ../Plugins/Swipe/Intermediate
+
+exit 0
